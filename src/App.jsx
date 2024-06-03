bb7ac7dccb3d2845cd8a652e73cba07bf390d22f
Caleb Siegel <caleb.siegel@gmail.com>
2024-06-03 09:37:53 -0400

    build app, navbar and add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,75 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { Container, Typography } from '@mui/material';
+import Navbar from './Navbar';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+  // const [user, setUser] = useState(null);
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   fetch(`/api/check_session`).then((res) => {
+  //       if (res.ok) {
+  //           res.json().then((user) => setUser(user));
+  //       }
+  //   });
+  // }, []);
+
+  // function attemptLogin(userInfo) {
+  //   fetch(`/api/login`, {
+  //       method: "POST",
+  //       headers: {
+  //           "Content-Type": "application/json",
+  //           "Accepts": "application/json",
+  //       },
+  //       body: JSON.stringify(userInfo),
+  //   })
+  //       .then((res) => {
+  //           if (res.ok) {
+  //               return res.json();
+  //           }
+  //           throw res;
+  //       })
+  //       .then((data) => {
+  //           setUser(data);
+  //           console.log(data)
+  //           navigate("/recipes");
+  //       })
+  //       .catch((e) => {
+  //           alert('incorrect username or password')
+  //           console.log(e);
+  //       });
+  // }
+  // function logout() {
+  //   fetch(`/api/logout`, { method: "DELETE" }).then((res) => {
+  //       if (res.ok) {
+  //           setUser(null);
+  //       }
+  //   });
+  // }
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.body.classList.toggle('dark-mode');
+  };
+
   return (
     <>
+      <Container disableGutters maxWidth={false} sx={{ height: '100vh', width: '100%', padding: '0px'}}>
+        <Typography component={'span'}>
+          <Navbar toggleDarkMode={toggleDarkMode} darkMode={darkMode}/>
+          <Outlet />
+        </Typography>
+      </Container>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/Navbar.jsx
@@ -0,0 +1,47 @@
+import { NavLink } from "react-router-dom";
+import { Container, TextField, Autocomplete } from "@mui/material";
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+
+function Navbar({ darkMode, toggleDarkMode }) {
+
+    return (
+        <Container disableGutters maxWidth={false} className="navbar">
+            <Grid container alignItems="center" justifyContent="space-between">
+                <Grid item>
+                    <Box display="flex">
+                        <NavLink to="/" className="nav-link">Home</NavLink>
+                        {/* <NavLink to="recipes" className="nav-link">Recipes</NavLink> */}
+                        {/* <NavLink to="mealprep" className="nav-link">Meal Prep</NavLink> */}
+                        {/* <NavLink to="random" className="nav-link">Spice It Up</NavLink> */}
+                        {/* <NavLink to="newtag" className="nav-link">Add New Tag</NavLink> */}
+                        {/* {user && user.id && <NavLink to={`user/${user.id}`} className="nav-link">Profile Page</NavLink>} */}
+                    </Box>
+                </Grid>
+                <Grid item>
+                    <Box display="flex" alignItems="center" sx={{ justifyContent: 'space-between', gap: '8px' }}>
+                        {/* {!user ? (
+                            <NavLink to="login" className="nav-link">Login</NavLink>
+                        ) : (
+                            <Box>
+                                <NavLink to="/" className="nav-link" onClick={logout}>Logout</NavLink>
+                                <span>Welcome, {user.name}</span>
+                            </Box>
+                        )} */}
+                        <FormGroup>
+                            <FormControlLabel
+                                control={<Switch defaultChecked onChange={toggleDarkMode} />}
+                                label={darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+                            />
+                        </FormGroup>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Container>
+    );
+};
+
+export default Navbar;
