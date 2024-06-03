e7fce9259c65dabaa584f740d1830581652e45e5
Caleb Siegel <caleb.siegel@gmail.com>
2024-06-03 09:07:43 -0400

    build index file

---
+++ src/index.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // errorElement: <Error />,
+    // children: [
+    //   {
+    //     path: "/",
+    //     element: <Home />
+    //   },
+    //   {
+    //     path: "login",
+    //     element: <Login />,
+    //   },
+    //   {
+    //     path: "recipes/:id",
+    //     element: <IndividualRecipe />,
+    //   },
+    //   {
+    //     path: "user/:id",
+    //     element: <ProfilePage />,
+    //   }
+    // ]
+  }
+])
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#00C49A",
+      // light: "",
+      // dark: "",
+      // contrastText: "#",
+    },
+    secondary: {
+      main: '#156064'
+    },
+    // error: {
+    //   main: '#'
+    // },
+    // warning: {
+    //   main: '#'
+    // },
+    info: {
+      main: '#F8E16C'
+    },
+    success: {
+      main: '#FFC2B4'
+    },
+    background: {
+      main: '#FB8F67'
+    },
+    // text: {
+    //   main: '#'
+    // },
+  },
+  typography: {
+    h1: {
+      fontSize: "3rem",
+      fontWeight: 600,
+    },
+    h2: {
+      fontSize: "1.75rem",
+      fontWeight: 600,
+    },
+    h3: {
+      fontSize: "1.5rem",
+      fontWeight: 600,
+    },
+  },
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+  </React.StrictMode>
+);
