0e3a37ef10dc2405100112b2ed5369b7bc2cb6eb
Caleb Siegel <caleb.siegel@gmail.com>
2024-11-12 08:23:46 -0500

    allow for refreshing single ticket

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,150 +1,162 @@
-import React, { useState, useEffect } from 'react'
-import Table from '../table/Table'
-import Slider from '../slider/Slider'
+import React, { useState, useEffect } from "react";
+import Table from "../table/Table";
+import Slider from "../slider/Slider";
 import "./home.css";
-import Search from '../search/Search';
-import ShowSkeleton from './ShowSkeleton';
-import Filter from '../filter/Filter';
-import Categories from '../categories/Categories';
+import Search from "../search/Search";
+import ShowSkeleton from "./ShowSkeleton";
+import Filter from "../filter/Filter";
+import Categories from "../categories/Categories";
 
 function Home() {
-    const [loading, setLoading] = useState(true)
-    const [shows, setShows] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [shows, setShows] = useState([]);
 
-    const [category, setCategory] = useState("broadway")
-    const handleSetCategory = (event) => {
-        setCategory(event.target.value)
-        setLoading(true)
-    }
+  const [category, setCategory] = useState("broadway");
+  const handleSetCategory = (event) => {
+    setCategory(event.target.value);
+    setLoading(true);
+  };
 
-    useEffect(() => {
-        fetch(`https://broadwaycommunity-backend.vercel.app/api/categories/${category}`)
-        .then((response) => response.json())
-        .then((data) => {
-            setShows(data);
-            setLoading(false);
-        });
-    }, [category]);
-    
-    const refreshData = () => {
-        console.log("fetch is running")
-        setLoading(true);
-        fetch(`https://broadwaycommunity-backend.vercel.app/api/fetch_tickets`, {
-            method: 'POST',
-        })
-        .then((response) => response.json())
-        .then(() => {
-            fetch(`https://broadwaycommunity-backend.vercel.app/api/categories/${category}`)
-                .then((response) => response.json())
-                .then((data) => {
-                    setShows(data);
-                    setLoading(false);
-                });
-        })
-        .catch((error) => {
-            console.error('Error refreshing data:', error);
-            setLoading(false);
-        });
-    };
-
-    const [searchTerm, setSearchTerm] = useState("")
-    
-    const handleSearchTerm = (event) => {
-        event.preventDefault();
-        setSearchTerm(event.target.value)
-    }
-    
-    let filteredShows = shows.event
-    if (shows?.event?.length > 1) {
-        filteredShows = shows?.event?.sort((a, b) => a?.event_info[0]?.price - b?.event_info[0]?.price);
-    }
-    
-    const [active, setActive] = useState("all");
-
-    const handleActive = (event) => {
-        event.preventDefault();
-        setActive(event.target.value)
-    }
-    
-    if (active === "all") {
-        filteredShows = shows.event
-    } else if (active === "today") {
-        const today = new Date().toISOString().split('T')[0];
-        filteredShows = filteredShows?.filter(show => {
-            const showDate = show?.event_info[0]?.event_date;
-            if (!showDate) {
-                console.error("Invalid date:", showDate);
-                return false;
-            }
-            return showDate === today;
-        });    
-    } else if (active === "7days") {
-        const today = new Date();
-        const sevenDaysFromNow = new Date(today);
-        sevenDaysFromNow.setDate(today.getDate() + 7);
-        const formattedSevenDaysFromNow = sevenDaysFromNow.toISOString().split('T')[0];
-
-        filteredShows = filteredShows?.filter(show => {
-            const showDate = show?.event_info[0]?.event_date;
-            if (!showDate) {
-                console.error("Invalid date:", showDate);
-                return false;
-            }
-            return showDate <= formattedSevenDaysFromNow;
-        });
-    } else if (active === "cheapest") {
-        filteredShows = filteredShows.sort((a, b) => a.event_info[0].price - b.event_info[0].price);
-    }
-    
-    filteredShows = filteredShows?.filter(show => {
-        return (
-            show && 
-            show.name &&
-            (
-                show.event_info[0]?.name?.toLowerCase().includes(searchTerm.toLowerCase())
-                ||
-                show.name.toLowerCase().includes(searchTerm.toLowerCase())
-            )
-        );
-    })
-
-    return (
-        <section className="home section" id="home">
-            <div className="home__container container grid">
-                <div className="home__content grid">
-                    <div className="home__data">
-                        {/* <h1 className="home__title">Attend the Event for Cheap</h1> */}
-                        <h1 className="home__title">Find Tickets To  
-                            <Categories category={category} handleSetCategory={handleSetCategory}/>
-                        </h1>
-                        <p className="home__description">Find the cheapest available ticket on Stubhub for each event listed below. Note that each ticket will cost an additional ~30% for Stubhub's fee.</p>
-                        
-                        <div className="home__filters">
-                            
-                            <div className='home__top-filters'>
-                                
-                                <Search searchTerm={searchTerm} handleSearchTerm={handleSearchTerm}/>
-                                <button className="home__refresh-button" onClick={refreshData}> <i class='bx bx-refresh home__refresh-button-icon'></i> Refresh Data</button>
-                            </div>
-                            <Filter active={active} handleActive={handleActive} />
-                            
-                        </div>
-                        
-                        {!loading
-                            ?
-                                <>
-                                <Slider shows={filteredShows}/>
-                                </>
-                                
-                            :
-                                <ShowSkeleton />
-                        }
-                        {/* <Table shows={filteredShows}/> */}
-                    </div>
-                </div>
-            </div>
-        </section>
+  useEffect(() => {
+    fetch(
+      `https://broadwaycommunity-backend.vercel.app/api/categories/${category}`
     )
+      .then((response) => response.json())
+      .then((data) => {
+        setShows(data);
+        setLoading(false);
+      });
+  }, [category]);
+
+  const refreshData = () => {
+    console.log("fetch is running");
+    setLoading(true);
+    fetch(`https://broadwaycommunity-backend.vercel.app/api/fetch_tickets`, {
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .then(() => {
+        setCategory(category);
+      })
+      .catch((error) => {
+        console.error("Error refreshing data:", error);
+        setLoading(false);
+      });
+  };
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchTerm = (event) => {
+    event.preventDefault();
+    setSearchTerm(event.target.value);
+  };
+
+  let filteredShows = shows.event;
+  if (shows?.event?.length > 1) {
+    filteredShows = shows?.event?.sort(
+      (a, b) => a?.event_info[0]?.price - b?.event_info[0]?.price
+    );
+  }
+
+  const [active, setActive] = useState("all");
+
+  const handleActive = (event) => {
+    event.preventDefault();
+    setActive(event.target.value);
+  };
+
+  if (active === "all") {
+    filteredShows = shows.event;
+  } else if (active === "today") {
+    const today = new Date().toISOString().split("T")[0];
+    filteredShows = filteredShows?.filter((show) => {
+      const showDate = show?.event_info[0]?.event_date;
+      if (!showDate) {
+        console.error("Invalid date:", showDate);
+        return false;
+      }
+      return showDate === today;
+    });
+  } else if (active === "7days") {
+    const today = new Date();
+    const sevenDaysFromNow = new Date(today);
+    sevenDaysFromNow.setDate(today.getDate() + 7);
+    const formattedSevenDaysFromNow = sevenDaysFromNow
+      .toISOString()
+      .split("T")[0];
+
+    filteredShows = filteredShows?.filter((show) => {
+      const showDate = show?.event_info[0]?.event_date;
+      if (!showDate) {
+        console.error("Invalid date:", showDate);
+        return false;
+      }
+      return showDate <= formattedSevenDaysFromNow;
+    });
+  } else if (active === "cheapest") {
+    filteredShows = filteredShows.sort(
+      (a, b) => a.event_info[0].price - b.event_info[0].price
+    );
+  }
+
+  filteredShows = filteredShows?.filter((show) => {
+    return (
+      show &&
+      show.name &&
+      (show.event_info[0]?.name
+        ?.toLowerCase()
+        .includes(searchTerm.toLowerCase()) ||
+        show.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    );
+  });
+
+  return (
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <div className="home__data">
+            {/* <h1 className="home__title">Attend the Event for Cheap</h1> */}
+            <h1 className="home__title">
+              Find Tickets To
+              <Categories
+                category={category}
+                handleSetCategory={handleSetCategory}
+              />
+            </h1>
+            <p className="home__description">
+              Find the cheapest available ticket on Stubhub for each event
+              listed below. Note that each ticket will cost an additional ~30%
+              for Stubhub's fee.
+            </p>
+
+            <div className="home__filters">
+              <div className="home__top-filters">
+                <Search
+                  searchTerm={searchTerm}
+                  handleSearchTerm={handleSearchTerm}
+                />
+                <button className="home__refresh-button" onClick={refreshData}>
+                  {" "}
+                  <i class="bx bx-refresh home__refresh-button-icon"></i>{" "}
+                  Refresh Data
+                </button>
+              </div>
+              <Filter active={active} handleActive={handleActive} />
+            </div>
+
+            {!loading ? (
+              <>
+                <Slider shows={filteredShows} />
+              </>
+            ) : (
+              <ShowSkeleton />
+            )}
+            {/* <Table shows={filteredShows}/> */}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,82 +1,158 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 import "./slider.css";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
-import { Mousewheel, FreeMode, Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import {
+  Mousewheel,
+  FreeMode,
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+} from "swiper/modules";
 
 const Slider = ({ shows }) => {
-  const generateWhatsAppLink = (show) => {
-    const message = `${show.event_info[0]?.name}: $${show.event_info[0]?.price}
-${show.event_info[0]?.formatted_date}
-${show.event_info[0]?.link}`;
+  const [localShows, setLocalShows] = useState(shows);
+  //   const generateWhatsAppLink = (show) => {
+  //     const message = `${show.event_info[0]?.name}: $${show.event_info[0]?.price}
+  // ${show.event_info[0]?.formatted_date}
+  // ${show.event_info[0]?.link}`;
 
-    // Encode the message for WhatsApp URL
-    return `https://wa.me/?text=${encodeURIComponent(message)}`;
-    };
-    return (
-      <section className="slider container section" id='slider'>
-        {/* <h2 className="section__title">Shows</h2> */}
-        {/* <span className="section__subtitle">What Makes Me... Me </span> */}
+  //     // Encode the message for WhatsApp URL
+  //     return `https://wa.me/?text=${encodeURIComponent(message)}`;
+  //     };
 
-        <Swiper className="slider__container"
-            modules={[ Mousewheel, FreeMode, Pagination ]}
-            spaceBetween={24}
-            slidesPerView={2}
-            // autoHeight={false}
-            freeMode={true}
-            // loop={true}
-            grabCursor={true}
-            centeredSlides={true}
-            mousewheel={true}
-            breakpoints={{
-                350: {
-                    slidesPerView: 1.5,
-                    spaceBetween: 24,
-                  },
-                  576: {
-                    slidesPerView: 1.5,
-                    spaceBetween: 24,
-                  },
-                  768: {
-                    slidesPerView: 2,
-                    spaceBetween: 48,
-                  },
-            }}
-            pagination={{ clickable: true, }}
-        >
-            {shows && shows.map((show) => {
-                return (
-                  show.event_info[0] && <SwiperSlide className='slider__card' key={show.id}>
-                      {!(show.category_id === 1) && <p className="slider__description-subheader">{show.name}</p>}
-                      {/* <img src={show.image} alt="" className="slider__img" /> */}
-                      
-                      <h3 className="slider__name">{show.event_info[0]?.name}
-                          <i className={show.image}></i>
-                          <a
-                            href={generateWhatsAppLink(show)}
-                            className="slider__button slider__button--whatsapp"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Share <i class='bx bx-share'></i>
-                        </a>
-                      </h3>
-                      <p className="slider__description-price">${show.event_info[0]?.price}</p>
-                      <p className="slider__description-theater">{show.venue?.name}</p>
-                      <p className="slider__description-date">{show.event_info[0]?.formatted_date}</p>
-                      
-                      <a href={show.event_info[0]?.link} className="slider__button" target="_blank" rel="noopener noreferrer">
-                          Buy Now <i className="bx bx-right-arrow-alt slider__button-icon"></i>
-                      </a>
-                      
-                  </SwiperSlide>
-                )
-            })}
-        </Swiper>
-      </section>
+  const refreshData = (id) => {
+    console.log("fetch is running");
+    // setLoading(true);
+    fetch(
+      `https://broadwaycommunity-backend.vercel.app/api/fetch_ticket/${id}`,
+      {
+        method: "POST",
+      }
     )
-}
+      .then((response) => response.json())
+      .then((updatedShowArray) => {
+        const updatedShow = updatedShowArray[0];
+        console.log(shows);
+        console.log(updatedShow);
 
-export default Slider
+        setLocalShows((prevShows) =>
+          prevShows.map((show) =>
+            show.id !== updatedShow.event_id
+              ? show
+              : {
+                  category_id: updatedShow.event.category_id,
+                  event_info: [updatedShow],
+                  event_preferences: updatedShow.event.event_preferences,
+                  id: updatedShow.event.id,
+                  image: updatedShow.event.image,
+                  lottery_url: updatedShow.event.lottery_url,
+                  name: updatedShow.event.name,
+                  show_duration: updatedShow.event.show_duration,
+                  stubhub_category_id: updatedShow.event.stubhub_category_id,
+                  venue: updatedShow.event.venue,
+                  venue_id: updatedShow.event.venue_id,
+                }
+          )
+        );
+      })
+      .catch((error) => {
+        console.error("Error refreshing data:", error);
+        // setLoading(false);
+      });
+  };
+
+  return (
+    <section className="slider container section" id="slider">
+      {/* <h2 className="section__title">Shows</h2> */}
+      {/* <span className="section__subtitle">What Makes Me... Me </span> */}
+
+      <Swiper
+        className="slider__container"
+        modules={[Mousewheel, FreeMode, Pagination]}
+        spaceBetween={24}
+        slidesPerView={2}
+        // autoHeight={false}
+        freeMode={true}
+        // loop={true}
+        grabCursor={true}
+        centeredSlides={true}
+        mousewheel={true}
+        breakpoints={{
+          350: {
+            slidesPerView: 1.5,
+            spaceBetween: 24,
+          },
+          576: {
+            slidesPerView: 1.5,
+            spaceBetween: 24,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+          },
+        }}
+        pagination={{ clickable: true }}
+      >
+        {localShows &&
+          localShows.map((show) => {
+            return (
+              show.event_info[0] && (
+                <SwiperSlide className="slider__card" key={show.id}>
+                  {!(show.category_id === 1) && (
+                    <p className="slider__description-subheader">{show.name}</p>
+                  )}
+                  {/* <img src={show.image} alt="" className="slider__img" /> */}
+
+                  <h3 className="slider__name">
+                    {show.event_info[0]?.name}
+                    <i className={show.image}></i>
+                    <button
+                      className="slider__refresh-button"
+                      onClick={() => refreshData(show.id)}
+                    >
+                      {" "}
+                      <i className="bx bx-refresh home__refresh-button-icon"></i>
+                    </button>
+                    {/* <a
+                                  href={generateWhatsAppLink(show)}
+                                  className="slider__button slider__button--whatsapp"
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                >
+                                    Share <i class='bx bx-share'></i>
+                                </a> */}
+                  </h3>
+                  <p className="slider__description-price">
+                    ${show.event_info[0]?.price}
+                  </p>
+                  {/* <p className="slider__description-theater">
+                    {show.venue?.name}
+                  </p> */}
+                  <p className="slider__description-date">
+                    {show.event_info[0]?.formatted_date}
+                  </p>
+
+                  <a
+                    href={show.event_info[0]?.link}
+                    className="slider__button"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Buy Now{" "}
+                    <i className="bx bx-right-arrow-alt slider__button-icon"></i>
+                  </a>
+                </SwiperSlide>
+              )
+            );
+          })}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Slider;
