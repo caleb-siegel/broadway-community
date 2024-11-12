59ccd98792f42c9ce5b83b6e17dac25e05f7e5b2
Caleb Siegel <caleb.siegel@gmail.com>
2024-11-12 09:28:44 -0500

    move error page text lower than the header

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,13 +3,15 @@ import Header from "../header/Header";
 import { Container, Typography } from "@mui/material";
 
 function Error() {
-    return (
-        <Container>
-            <Header /> 
-            <Typography variant="h1">The page you are looking for doesn't exist. Please try again.</Typography>
-            {/* <h1>The page you are looking for doesn't exist. Please try again.</h1> */}
-        </Container>
-    )
+  return (
+    <Container style={{ marginTop: "100px" }}>
+      <Header />
+      <Typography variant="h1">
+        The page you are looking for doesn't exist. Please try again.
+      </Typography>
+      {/* <h1>The page you are looking for doesn't exist. Please try again.</h1> */}
+    </Container>
+  );
 }
 
 export default Error;
