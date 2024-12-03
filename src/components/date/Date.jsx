7b4718a2bfd768809e9e88b93c1fb3940dd36620
Caleb Siegel <caleb.siegel@gmail.com>
2024-12-03 18:48:24 -0500

    minor text edit in date page

--- src/components/date/Date.jsx
+++ src/components/date/Date.jsx
@@ -153,7 +153,7 @@ function Date() {
             </h1>
             <p className="date__description">
               Choose a date range and click "See Prices" to find the cheapest available ticket on Stubhub for each event
-              listed below. Note that each ticket will cost an{" "}
+              listed below in that date range. Note that each ticket will cost an{" "}
               <strong>additional ~30% for Stubhub's fee.</strong>
             </p>
 
