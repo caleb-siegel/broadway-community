4ba6dd05c3f7b2cfad7a33ae041452cdcf7c7969
Caleb Siegel <caleb.siegel@gmail.com>
2024-12-04 08:06:43 -0500

    move category options to categories component from home

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "./categories.css";
 
-const Categories = ({ category, handleSetCategory, categoryOptions }) => {
+const Categories = ({ category, handleSetCategory }) => {
+  
+  const [categoryOptions, setCategoryOptions] = useState([]);
+  useEffect(() => {
+    fetch(
+      `${backendUrl}/api/category_names`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setCategoryOptions(data);
+      });
+  }, []);
+
   return (
     <div className="categories__container">
       <label className="categories__label"></label>
